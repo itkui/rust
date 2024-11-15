633116643d1fcfaadeb602a4744038c2d03aa278
itkui <itkui2018@gmail.com>
2024-11-15 14:31:50 +0800

    feat: 字符类型

--- 3.normal/data-type/src/main.rs
+++ 3.normal/data-type/src/main.rs
@@ -72,7 +72,7 @@ fn main() {
 
     /**
      * 字符类型
-     * char
+     * char 我们用单引号声明 char 字面量，而与之相反的是，使用双引号声明字符串字面量
      */
     let c = 'z';
     let z: char = 'ℤ'; // with explicit type annotation
